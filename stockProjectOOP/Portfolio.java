a97a17f6090617555a2b26f61f43e51c9cb0730a
Chris Addeo <cda8682@nyu.edu>
2023-12-01 12:45:57 -0500

    Updated Portfolio.java to have cleaner comments

--- stockProjectOOP/Portfolio.java
+++ stockProjectOOP/Portfolio.java
@@ -8,44 +8,39 @@ public class Portfolio {
     private List<PortfolioItem> items;
 
     /*
-     * Purpose: Initializes the Portfolio instance.
-     * Process: Sets up an empty list (ArrayList) of PortfolioItem objects to store
-     * the user's portfolio items.
+     * Constructor for Portfolio class.
+     * Initializes the Portfolio instance with an
+     * empty list of PortfolioItem objects.
+     * 
      */
     public Portfolio() {
         this.items = new ArrayList<>();
     }
 
-    /*
-     * Input: PortfolioItem item - the PortfolioItem object to add to the portfolio.
-     * Purpose: Adds a new stock item to the user's portfolio.
-     * Process: Appends the given PortfolioItem to the items list.
-     * Output: None.
+    /**
+     * Adds a PortfolioItem to the portfolio.
+     * 
+     * @param item The PortfolioItem object to be added.
      */
     public void addPortfolioItem(PortfolioItem item) {
         this.items.add(item);
     }
 
-    /*
-     * Input: PortfolioItem item - the PortfolioItem object to remove from the
-     * portfolio.
-     * Purpose: Removes a specified stock item from the user's portfolio.
-     * Process: Attempts to remove the given PortfolioItem from the items list.
-     * Output: boolean - Returns true if the item was successfully removed, false
-     * otherwise.
+    /**
+     * Removes a PortfolioItem from the portfolio.
+     * 
+     * @param item The PortfolioItem object to be removed.
+     * @return true if the item was successfully removed, false otherwise.
      */
     public boolean removePortfolioItem(PortfolioItem item) {
         return this.items.remove(item);
     }
 
-    /*
-     * Input: String stockName - the name of the stock to search for in the
-     * portfolio.
-     * Purpose: Retrieves a specific PortfolioItem based on the stock name.
-     * Process: Iterates through the items list and returns the PortfolioItem with a
-     * matching stock name.
-     * Output: PortfolioItem - Returns the found item, or null if no matching item
-     * is found.
+    /**
+     * Retrieves a specific PortfolioItem based on the stock name.
+     * 
+     * @param stockName The name of the stock.
+     * @return The PortfolioItem with the matching stock name, or null if not found.
      */
     public PortfolioItem getPortfolioItem(String stockName) {
         for (PortfolioItem item : this.items) {
@@ -56,12 +51,10 @@ public class Portfolio {
         return null; // Return null if the item is not found
     }
 
-    /*
-     * Input: None.
-     * Purpose: Calculates the total current value of all stocks in the portfolio.
-     * Process: Sums up the current value (getCurrentValue()) of each PortfolioItem
-     * in the items list.
-     * Output: double - The total value of the portfolio.
+    /**
+     * Calculates the total current value of all stocks in the portfolio.
+     * 
+     * @return The total value of the portfolio.
      */
     public double getTotalValue() {
         double totalValue = 0.0;
@@ -71,15 +64,11 @@ public class Portfolio {
         return totalValue;
     }
 
-    /*
-     * Input: None.
-     * Purpose: Calculates the total profit or loss of the portfolio since the
-     * stocks were purchased.
-     * Process: For each PortfolioItem, calculates the difference between its
-     * current value and the total purchase price (purchase price per share
-     * multiplied by the number of shares owned). Sums up these differences to
-     * determine the overall profit or loss.
-     * Output: double - The total profit or loss of the portfolio.
+    /**
+     * Calculates the total profit or loss of the portfolio
+     * since the stocks were purchased.
+     * 
+     * @return The total profit or loss of the portfolio.
      */
     public double getProfitOrLoss() {
         double profitOrLoss = 0.0;
@@ -91,15 +80,12 @@ public class Portfolio {
         return profitOrLoss;
     }
 
-    /*
-     * Input: None.
-     * Purpose: Provides a list of all PortfolioItem objects in the portfolio.
-     * Process: Returns a new list (ArrayList) containing all PortfolioItem objects
-     * from the items list. This approach ensures that the original list cannot be
-     * modified directly, preserving data integrity.
-     * Output: List<PortfolioItem> - A list of all portfolio items.
+    /**
+     * Provides a list of all PortfolioItem objects in the portfolio.
+     * 
+     * @return A list of all portfolio items.
      */
     public List<PortfolioItem> getPortfolioItems() {
-        return new ArrayList<>(this.items); // Return a copy for immutability
+        return new ArrayList<>(this.items); // Return a copy
     }
 }
