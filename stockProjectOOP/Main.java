54bb853961c8cc1cb90e86308900375ad338c432
AaryanVirani <viraniaaryan@gmail.com>
2023-12-06 10:13:52 -0500

    Added commenting + main class

---
+++ stockProjectOOP/Main.java
@@ -0,0 +1,8 @@
+package stockProjectOOP;
+
+public class Main {
+    public static void main(String[] args) {
+        MainInterface mf = new MainInterface();
+        mf.setVisible(true);
+    }
+}
